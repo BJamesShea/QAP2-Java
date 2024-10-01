350e7988971019c408981a94d67d103728012b7a
Brandon Shea <brandon.shea1994@icloud.com>
2024-10-01 18:26:45 -0230

    Restarting project after switching IDEs

--- TestMyLine.java
+++ TestMyLine.java
@@ -1,3 +1,7 @@
 public class TestMyLine {
-    
+
+    public static void main(String[] args) {
+    }
 }
+    
+
---
+++ TestMyPoint.java
@@ -0,0 +1,4 @@
+public class TestMyPoint {
+    public static void main(String[] args) {}
+
+}
