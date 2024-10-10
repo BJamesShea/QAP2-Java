95df6808a01b7c12658fc947211724f1cc76c50d
Brandon Shea <brandon.shea1994@icloud.com>
2024-10-10 20:06:55 -0230

    Person class

--- Problem3/Person.java
+++ Problem3/Person.java
@@ -1,4 +1,18 @@
 package Problem3;
 
 public class Person {
+    private final String lastName;
+    private final String firstName;
+    private final Address home;
+
+    public Person(String lastName, String firstName, Address home) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.home = home;
+    }
+
+    public String toString() {
+        return firstName + " " + lastName + " " + home.toString();
+
+    }
 }
