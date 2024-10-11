baf5d1b78c01f69a2b2310cde5212bdbd62dd1ee
Brandon Shea <brandon.shea1994@icloud.com>
2024-10-10 23:38:24 -0230

    Final commit

--- Problem3/CreditCardDemo.java
+++ Problem3/CreditCardDemo.java
@@ -1,4 +1,30 @@
 package Problem3;
 
 public class CreditCardDemo {
+    public static void main(String[] args) {
+
+        Address address = new Address("4 Rockstar Road", "Austin", "NH", "40254");
+        Person owner = new Person("Morgan", "Arthur", address);
+
+
+        Money creditLimit = new Money(1000, 0);
+
+
+        CreditCard card = new CreditCard(owner, creditLimit);
+
+
+        Money chargeAmount1 = new Money(200, 0);
+        System.out.println("Attempting to charge $200...");
+        card.Charge(chargeAmount1);
+        System.out.println("Current balance: " + card.getBalance().toString());
+
+
+        Money paymentAmount1 = new Money(50, 0);
+        System.out.println("Attempting to make a $50 payment...");
+        card.Payment(paymentAmount1);
+        System.out.println("Current balance: " + card.getBalance().toString());
+
+
+        System.out.println("Card owner info: " + card.getOwner().toString());
+    }
 }
